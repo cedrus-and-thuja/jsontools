125d8afa5750f2d417f0a14359ef291726af530f
Paul J DeCoursey <paul@decoursey.net>
2024-06-06 17:40:25 -0500

    go flag fix

--- cmd/janus/main.go
+++ cmd/janus/main.go
@@ -16,7 +16,7 @@ func main() {
 	var generateKotlin bool
 
 	flag.StringVar(&schemaFileLocation, "s", "", "location of schema file, required")
-	flag.BoolVar(&generateGo, "go", true, "generate go structs, defaults to true")
+	flag.BoolVar(&generateGo, "go", false, "generate go structs, defaults to false")
 	flag.BoolVar(&generateKotlin, "kotlin", false, "generate kotlin classes, defaults to false")
 	flag.Parse()
 
@@ -32,21 +32,25 @@ func main() {
 	}
 	schema, err := compiler.Compile("schema.json")
 	if err != nil {
-		fmt.Printf("error parsing schema: %s", err)
+		fmt.Printf("error parsing schema: %s\n, err)
 		os.Exit(1)
+	}
+	if !generateGo && !generateKotlin {
+		fmt.Printf("nothing generated, schema was parsed successfully\n")
+		os.Exit(4)
 	}
 	if generateGo {
 		err = generator.Construct(schema, generator.NewConfig(), generator.GO)
 		if err != nil {
-			fmt.Printf("error generating go structs: %s", err)
+			fmt.Printf("error generating go structs: %s\n", err)
 			os.Exit(2)
 		}
 	}
 	if generateKotlin {
 		err = generator.Construct(schema, generator.NewConfig(), generator.KOTLIN)
 		if err != nil {
-			fmt.Printf("error generating kotlin classes: %s", err)
-			os.Exit(2)
+			fmt.Printf("error generating kotlin classes: %s\n", err)
+			os.Exit(3)
 		}
 	}
 }
